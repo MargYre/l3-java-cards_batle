dc84a3b9f19f26c8df35d276002fd03aba33bee5
margaux <margaux.tarayre@gmail.com>
2024-10-04 10:45:26 +0200

    Add the draft of the class Carte

---
+++ CardsBattle.java
@@ -0,0 +1,16 @@
+public class Carte {
+    public Carte() {
+        String[] couleur = {"rouge", "noir"};
+        int[] valeur = {1, 2, 3,4,5,6,7,8,9,10,11,13,14};
+    }
+
+    private accesseurs() {
+        this.couleur;
+        this.valeur;
+    }
+    private modificateurs(color, value) {
+        this.couleur = color;
+        this.valeur = value;
+    }
+}
+
